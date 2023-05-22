e69c0542404cbc5a168e963229e870e7e254df79
Martín <magadiflo@gmail.com>
2023-05-21 22:56:14 -0500

    Creación de la vista para el tipo de cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversionComponent } from './conversion/conversion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/conversion/conversion.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversion',
+  templateUrl: './conversion.component.html',
+  styleUrls: ['./conversion.component.scss']
+})
+export class ConversionComponent {
+
+  guardar() {
+
+  }
+
+}
